3f2ae1e9861e2c6ce6606954f5207584e3ec09c7
mahesh <maheshpimple2002@gmail.com>
2025-01-27 22:25:42 +0530

    tracker: organized static files

--- project_tracker/settings.py
+++ project_tracker/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -114,9 +115,15 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
+STATIC_URL = '/static/'
 
-STATIC_URL = 'static/'
+# This is where your app's static files are located
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'timetracker', 'static'),
+]
+
+# This is where collectstatic will collect all static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
